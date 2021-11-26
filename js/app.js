5b0c68e78752e2516fd24d8a502a75519db51045
harging <gavin.hargin@gmail.com>
2021-11-26 18:03:28 +0000

    mvp done

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+  
+    document.querySelector("#new-item-form").addEventListener('submit', handleEvent);
+    
+    document.querySelector("#new-item-form").addEventListener('select', handleEvent);
+  
+    document.querySelector("#delete-button").addEventListener('click', handleDeleteAll);
+  });
+
+let list = document.querySelector("#games-list")
+
+const handleEvent = function (event) {
+    event.preventDefault()
+    const newListItem = document.createElement("li")
+    newListItem.textContent = 
+    `${event.target.title.value} - ${event.target.developer.value} - ${event.target.format.value}`
+    const list = document.querySelector("#games-list")
+    list.appendChild(newListItem)
+    document.querySelector("#new-item-form").reset()
+}
+
+const handleDeleteAll = function (event) {
+    const gamesList = document.querySelector('#games-list');
+    gamesList.innerHTML = '';
+}
